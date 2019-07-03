6fde50d349029a7a5d0a4afa17966e13b49e6e7f
bansaljas <jasbansal56@gmail.com>
2019-07-03 15:32:05 +0530

    Added Login and SignUp Template

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+
+#Here we are not regisering our model as it is inherited form django's User model
+#which is automatically registered and so our model is also. 
